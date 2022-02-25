30fce29c1ae0019122e6fc51e410a35db337a8da
pascall de creator <pascall.de.creator@gmail.com>
2022-02-25 16:05:15 +0000

    fix responsiveness

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-$(document).ready(()=>{
+// $(document).ready(()=>{
   
-  $('.advice-btn').on('click', ()=>{
-    $.get('https://api.adviceslip.com/advice', (data)=>{
-      let obj = JSON.parse(data)
-      let item = obj.slip
-      $('.card-advice-no').text(`advice #${item.id}`)
-      $('.card-quote').html(`&ldquo;${item.advice}&rdquo;`)
-  })
-  })
-})
+//   $('.advice-btn').on('click', ()=>{
+//     $.get('https://api.adviceslip.com/advice', (data)=>{
+//       let obj = JSON.parse(data)
+//       let item = obj.slip
+//       $('.card-advice-no').text(`advice #${item.id}`)
+//       $('.card-quote').html(`&ldquo;${item.advice}&rdquo;`)
+//   })
+//   })
+// })
+
+// js fetch method # not compulsery but its nice to know since jquery is not so relevant 
+var adviceText, reloadBtn
+adviceText = document.getElementById('advice')
+reloadBtn = document.getElementById('reloadBtn')
+
+// not complete
